f8645135b91b7a58ffd19d405f6f6c3bbdc87961
ch-kartik <kartikch619@gmail.com>
2025-06-08 00:16:23 +0530

    Changes pushed on June 8th 2025

---
+++ src/ex_01_Java_Basic/Lab004_Comments.java
@@ -0,0 +1,11 @@
+package ex_01_Java_Basic;
+
+public class Lab004_Comments {
+
+    public static void main(String[] args) {
+
+        //This is a comment and it will be ignored by the compiler
+
+        System.out.println("Hi");
+    }
+}
---
+++ src/ex_01_Java_Basic/Lab005_Comments.java
@@ -0,0 +1,8 @@
+package ex_01_Java_Basic;
+
+public class Lab005_Comments {
+    public static void main(String[] args) {
+        //This is also a comment
+        System.out.println("Hello World!");
+    }
+}
