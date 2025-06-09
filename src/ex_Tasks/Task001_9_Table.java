fbcbf1d10635b34ec6f3d9e9264fdd1bc57a227a
ch-kartik <kartikch619@gmail.com>
2025-06-09 22:01:44 +0530

    Changes pushed on June 9th 2025

---
+++ src/ex_Tasks/Task001_9_Table.java
@@ -0,0 +1,18 @@
+package ex_Tasks;
+
+public class Task001_9_Table {
+    public static void main(String[] args) {
+        byte n = 9;
+        System.out.print("Multiplication of table 9:\n");
+        System.out.printf("%d * 1 = %d\n",n,n*1);
+        System.out.printf("%d * 2 = %d\n",n,n*2);
+        System.out.printf("%d * 3 = %d\n",n,n*3);
+        System.out.printf("%d * 4 = %d\n",n,n*4);
+        System.out.printf("%d * 5 = %d\n",n,n*5);
+        System.out.printf("%d * 6 = %d\n",n,n*6);
+        System.out.printf("%d * 7 = %d\n",n,n*7);
+        System.out.printf("%d * 8 = %d\n",n,n*8);
+        System.out.printf("%d * 9 = %d\n",n,n*9);
+        System.out.printf("%d * 10 = %d\n",n,n*10);
+    }
+}
