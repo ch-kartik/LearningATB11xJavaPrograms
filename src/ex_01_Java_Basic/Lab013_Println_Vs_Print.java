b09fa9c648bfb387bc0619c681be04676f8b9db1
ch-kartik <kartikch619@gmail.com>
2025-06-09 08:20:28 +0530

    Changes pushed on June 9th 2025

---
+++ src/ex_01_Java_Basic/Lab013_Println_Vs_Print.java
@@ -0,0 +1,11 @@
+package ex_01_Java_Basic;
+
+public class Lab013_Println_Vs_Print {
+    public static void main(String[] args) {
+        System.out.println("Hello World!"); // print with new line
+        System.out.println("How are you doing?"); // print with new line
+
+        System.out.print("Hello World.");
+        System.out.print("How are you?"); //print in same line as "Hello World."
+    }
+}
