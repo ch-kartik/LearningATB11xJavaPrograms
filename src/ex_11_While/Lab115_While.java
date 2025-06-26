c0c4673566feb366d9b593624afc9309940852c3
ch-kartik <kartikch619@gmail.com>
2025-06-26 22:45:24 +0530

    Changes pushed on June 26th 2025

---
+++ src/ex_11_While/Lab115_While.java
@@ -0,0 +1,17 @@
+package ex_11_While;
+
+public class Lab115_While {
+    public static void main(String[] args) {
+        int i = 0; // init
+        while(i < 10) { // Condition (must be true to enter the loop)
+            //System.out.println("Start the program");
+            System.out.println(i);
+            i++; // updation
+        }
+
+        // I, C, U
+        for(int j = 0; j < 10; j++) {
+            System.out.println(j);
+        }
+    }
+}
---
+++ src/ex_11_While/Lab116_While_2.java
@@ -0,0 +1,11 @@
+package ex_11_While;
+
+public class Lab116_While_2 {
+    public static void main(String[] args) {
+        int mod = 1;
+        while(mod <= 15) {
+            System.out.println("New version will be released.");
+            mod ++;
+        }
+    }
+}
