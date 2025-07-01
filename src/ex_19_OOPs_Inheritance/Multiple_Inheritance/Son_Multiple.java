88246e95d9ad00871b24eee95c77c13beab62d7c
ch-kartik <kartikch619@gmail.com>
2025-07-01 19:47:55 +0530

    Changes pushed on July 1st 2025

--- src/ex_19_OOPs_Inheritance/Multiple_Inheritance/Son_Multiple.java
+++ src/ex_19_OOPs_Inheritance/Multiple_Inheritance/Son_Multiple.java
@@ -1,8 +1,47 @@
 package ex_19_OOPs_Inheritance.Multiple_Inheritance;
 
-public class Son_Multiple // extends Mother_Multiple, Father_Multiple {
+public class Son_Multiple  // extends Mother_Multiple, Father_Multiple {
 
     // You can't have multiple inheritance
     // You can't take from multiple people - Classes
 
+    // Java doesn't support multiple inheritance to avoid "Diamond Problem" and to maintain simplicity and clarity in inheritance hierarchy.
+
+    // DIAMOND PROBLEM
+    // Imagine a scenario with four classes: Class A, Class B, Class C and Class D
+    // Class A has a method named doSomething().
+    // Class B and Class C both inherit from Class A and might override or implement doSomething() in their own ways.
+    // Class D attempts to inherit from both Class B and Class C.
+    // Now, if an object of ClassD calls doSomething(), it becomes ambiguous which version of the method should be executed:
+    // the one from ClassB or the one from ClassC? This ambiguity is the core of the diamond problem.
+
 }
+    // Example:
+
+// This code demonstrates the concept, but Java will not allow ClassD to extend both ClassB and ClassC.
+
+class ClassA {
+    public void doSomething() {
+        System.out.println("Doing something from ClassA");
+    }
+}
+
+class ClassB extends ClassA {
+    @Override
+    public void doSomething() {
+        System.out.println("Doing something from ClassB");
+    }
+}
+
+class ClassC extends ClassA {
+    @Override
+    public void doSomething() {
+        System.out.println("Doing something from ClassC");
+    }
+}
+
+// This would cause a compile-time error in Java:
+// class ClassD extends ClassB, ClassC {
+//     // Ambiguity arises if doSomething() is called on ClassD object
+// }
+
